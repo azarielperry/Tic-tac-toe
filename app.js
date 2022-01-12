9d559cdf315304571d974dc5fdcb13b011b074fd
Azariel Perry <azarielperry>
2022-01-11 20:48:16 -0800

    Rules of the game is functional (Restart button not yet)

--- app.js
+++ app.js
@@ -1,11 +1,74 @@
 //HTML Elements
-const nextTurn = document.querySelector('.turn');
+const statusDiv = document.querySelector('.status');
 const restart = document.querySelector('.restart');
 const gameCells = document.querySelectorAll('.game-cell');
 
-//Game VAriable
+//Game Variable
 let gameIsLive = true;
 let xIsNext = true;
+let winner = null;
+
+// Game Constants
+const xSymbol ='×';
+const oSymbol= '◯';
+
+
+// Functions
+
+const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+
+const handleWin = (letter) => {
+  gameIsLive = false;
+  winner= letter;
+  if (letter === 'x') {
+    statusDiv.innerHTML = `${letterToSymbol(winner)} WINNER!`;
+  } else {
+    statusDiv.innerHTML = `<span>${letterToSymbol(winner)} WINNER!</span>`;
+  }
+}; 
+const checkGameStatus = () => {
+    const topLeft = gameCells[0].classList[1];
+    const topMiddle = gameCells[1].classList[1];
+    const topRight = gameCells[2].classList[1];
+    const middleLeft = gameCells[3].classList[1];
+    const middleMiddle = gameCells[4].classList[1];
+    const middleRight = gameCells[5].classList[1];
+    const bottomLeft = gameCells[6].classList[1];
+    const bottomMiddle = gameCells[7].classList[1];
+    const bottomRight = gameCells[8].classList[1];
+
+
+
+//Winner
+if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+    handleWin(topLeft);
+} else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+    handleWin(middleLeft);
+} else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+    handleWin(bottomLeft);
+} else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+    handleWin(topLeft);
+} else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+    handleWin(topMiddle);
+} else if (topRight && topRight === middleRight && topRight === bottomRight) {
+    handleWin(topRight);
+} else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+    handleWin(topLeft);
+} else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+    handleWin(topRight);
+} else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
+    gameIsLive = false;
+    statusDiv.innerHTML = 'DRAW!';
+} else {
+    xIsNext =!xIsNext;
+  if (xIsNext) {
+        statusDiv.innerHTML = `${xSymbol} Turn`;
+  } else {
+      statusDiv.innerHTML = `<span> ${oSymbol} Turn </span>`;
+  }
+}
+};
+
 
 
 // Event Handlers
@@ -23,10 +86,11 @@ const handleClickCell = (e) => {
 
     if (xIsNext) {
         classList.add('x');
-        xIsNext =!xIsNext;
+        checkGameStatus();
+        
     } else {
         classList.add('o');
-        xIsNext =!xIsNext;
+        checkGameStatus();
     }
     
 };
@@ -35,49 +99,4 @@ const handleClickCell = (e) => {
 restart.addEventListener('click', handleRestart);
 for(const gameCell of gameCells) {
     gameCell.addEventListener('click', handleClickCell)
-}
-
-// Variables
-const xSymbol ='×';
-const oSymbol= '◯';
-
-//DOM Elements
-
-
-
-
-
-
-
-
-
-// restart.addEventListener('click', handleRestart);
-
-// for (const gameCell of gameCell) {
-//   gameCell.addEventListener('click', handlegameCell)
-// }
-
-//   const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
-
-// const handleWin = (letter) => {
-//   gameIsLive = false;
-//   if (letter === 'x') {
-//     statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
-//   } else {
-//     statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
-//   }
-// }; 
-
-// const checkGameStatus = () => {
-//   const topLeft = gameCell[0].classList[1];
-//   const topMiddle = gameCell[1].classList[1];
-//   const topRight = gameCell[2].classList[1];
-//   const middleLeft = gameCell[3].classList[1];
-//   const middleMiddle = gameCell[4].classList[1];
-//   const middleRight = gameCell[5].classList[1];
-//   const bottomLeft = gameCell[6].classList[1];
-//   const bottomMiddle = gameCell[7].classList[1];
-//   const bottomRight = gameCell[8].classList[1];
-// }
-// console.log(letterToSymbol);
-
+} 
